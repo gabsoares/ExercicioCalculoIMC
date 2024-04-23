6e606b3f04f2d4f1e45d823433275b3a42062980
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 23:56:50 -0300

    Feature - Inclusão de switch case

--- ExercicioCalculoIMC/Program.cs
+++ ExercicioCalculoIMC/Program.cs
@@ -18,30 +18,33 @@ else
 
     IMC = peso / (altura * altura);
 
-    if (genero == 'M')
+    switch (genero)
     {
-        if (IMC < 20.7)
-            Console.WriteLine($"Abaixo do peso! IMC = {IMC}");
-        else if (IMC >= 20.7 && IMC < 26.5)
-            Console.WriteLine($"Peso ideal! IMC = {IMC}");
-        else if (IMC >= 26.6 && IMC < 27.8)
-            Console.WriteLine($"Pouco acima do peso! IMC = {IMC}");
-        else if (IMC > 27.9 && IMC < 31.1)
-            Console.WriteLine($"Acima do peso! IMC = {IMC}");
-        else
-            Console.WriteLine($"Obesidade! IMC = {IMC}");
-    }
-    else
-    {
-        if (IMC < 19.1)
-            Console.WriteLine($"Abaixo do peso! IMC = {IMC}");
-        else if (IMC >= 19.1 && IMC < 25.8)
-            Console.WriteLine($"Peso ideal! IMC = {IMC}");
-        else if (IMC >= 25.9 && IMC < 27.3)
-            Console.WriteLine($"Pouco acima do peso! IMC = {IMC}");
-        else if (IMC >= 27.4 && IMC < 32.3)
-            Console.WriteLine($"Acima do peso! IMC = {IMC}");
-        else
-            Console.WriteLine($"Obesidade! IMC = {IMC}");
+        case 'M':
+        case 'm':
+            if (IMC < 20.7)
+                Console.WriteLine($"Abaixo do peso! IMC = {IMC}");
+            else if (IMC >= 20.7 && IMC < 26.5)
+                Console.WriteLine($"Peso ideal! IMC = {IMC}");
+            else if (IMC >= 26.6 && IMC < 27.8)
+                Console.WriteLine($"Pouco acima do peso! IMC = {IMC}");
+            else if (IMC > 27.9 && IMC < 31.1)
+                Console.WriteLine($"Acima do peso! IMC = {IMC}");
+            else
+                Console.WriteLine($"Obesidade! IMC = {IMC}");
+            break;
+        case 'H':
+        case 'h':
+            if (IMC < 19.1)
+                Console.WriteLine($"Abaixo do peso! IMC = {IMC}");
+            else if (IMC >= 19.1 && IMC < 25.8)
+                Console.WriteLine($"Peso ideal! IMC = {IMC}");
+            else if (IMC >= 25.9 && IMC < 27.3)
+                Console.WriteLine($"Pouco acima do peso! IMC = {IMC}");
+            else if (IMC >= 27.4 && IMC < 32.3)
+                Console.WriteLine($"Acima do peso! IMC = {IMC}");
+            else
+                Console.WriteLine($"Obesidade! IMC = {IMC}");
+            break;
     }
 }
